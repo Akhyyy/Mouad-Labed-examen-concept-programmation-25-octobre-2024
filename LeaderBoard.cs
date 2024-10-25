aaf03758d5e1b408ebd7105b0fb0222818bd553f
Akhyyy <88566947+Akhyyy@users.noreply.github.com>
2024-10-25 14:34:41 -0400

    Add files via upload

---
+++ LeaderBoard.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Leaderboard : MonoBehaviour
+{
+    public void UpdateLeaderboard(int playerPosition)
+    {
+        
+    }
+
+    public void DisplayLeaderboard()
+    {
+        
+    }
+}
